0d08c9bc2572d97909403335c205e9a486f7c318
dayannn <dayannn@yandex.ru>
2018-11-04 12:40:52 +0300

    Spring files added + travis and codecov integration

---
+++ src/main/java/com/dayannn/RSOI2/Rsoi2Application.java
@@ -0,0 +1,12 @@
+package com.dayannn.RSOI2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rsoi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rsoi2Application.class, args);
+	}
+}
